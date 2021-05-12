58d41dacf6202a7f7db472e7da0b24a2d7f225e3
Marla Schulz <marlaa.schulz@gmail.com>
2021-05-12 15:01:57 -0600

    Complete iteration 2

--- lib/classroom.rb
+++ lib/classroom.rb
@@ -1,2 +1,23 @@
+require 'pry'
+
 class Classroom
+  attr_reader :subject, :capacity, :students
+  attr_writer :studentscd
+  def initialize (subject, capacity)
+    @subject = subject
+    @capacity = capacity
+    @students = []
+  end
+  def add_student(student)
+    @students << student
+  end
+  def yell_at_students
+    #binding.pry
+    uppercase_students = []
+    @students.each do |name|
+      #binding.pry
+      uppercase_students << name.upcase
+    end
+    return uppercase_students
+  end
 end
